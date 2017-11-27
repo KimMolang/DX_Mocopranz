c27be31303e70a0831d14396c08b446485d1ed4a
KimMolang <bluevill04@gmail.com>
2017-11-27 21:47:02 +0900

    파일 정리 및 속성 변경

--- Client/Code/MainGame.cpp
+++ Client/Code/MainGame.cpp
@@ -1,17 +1,6 @@
 #include "stdafx.h"
 #include "MainGame.h"
 
-#include <time.h>
-
-#include "Device.h"
-#include "SceneMgr.h"
-
-#include "Timer.h"
-#include "KeyMgr.h"
-
-extern HINSTANCE	g_hInst;
-extern HWND			g_hWnd;
-
 CMainGame::CMainGame()
 {
 
@@ -19,67 +8,7 @@ CMainGame::CMainGame()
 
 CMainGame::~CMainGame()
 {
-	Release();
+	
 }
 
-HRESULT CMainGame::Init()
-{
-	srand(unsigned int(time(nullptr)));
 
-#ifdef _DEBUG
-	FAILED_CHECK(CDevice::GetInstance()->Init(CDevice::WIN_MODE_WIN));
-#else
-	FAILED_CHECK(CDevice::GetInstance()->Init(CDevice::WIN_MODE_WIN));
-#endif
-	m_pDevice = CDevice::GetInstance();
-	NULL_CHECK_RETURN(m_pDevice, E_FAIL);
-
-	// Mgr ---------------------
-	m_pSceneMgr = CSceneMgr::GetInstance();
-	m_pTimer = CTimer::GetInstance();
-	m_pKeyMgr = CKeyMgr::GetInstance();
-	//m_pServerMgr = CServer::GetInstance();
-
-
-	// Mgr Init ----------------
-	m_pSceneMgr->Init(m_pDevice);
-	m_pSceneMgr->SetScene(CSceneMgr::SCENE_ID_INMAP);
-	m_pTimer->Init();
-	m_pKeyMgr->Init(g_hInst, g_hWnd);
-	////m_pServerMgr->InitServer(g_hWnd);
-
-
-	return S_OK;
-}
-
-void CMainGame::Update()
-{
-	m_pTimer->Update();
-	m_pKeyMgr->UpdateInputState();
-	m_pSceneMgr->Update();
-}
-
-void CMainGame::Render()
-{
-	m_pSceneMgr->Render();
-}
-
-void CMainGame::Release()
-{
-	m_pTimer->DestroyInstance();
-	m_pKeyMgr->DestroyInstance();
-	m_pSceneMgr->DestroyInstance();
-
-	//CObjMgr::GetInstance()->DestroyInstance();
-	//CColMgr::GetInstance()->DestroyInstance();
-	//CInMapMgr::GetInstance()->DestroyInstance();
-
-	m_pDevice->DestroyInstance();
-
-
-	//CInfoSubject::GetInstance()->DestroyInstance();
-	////m_pServerMgr->DestroyInstance();
-
-	//CResourceMgr::GetInstance()->DestroyInstance();
-	//CLoadMgr::GetInstance()->DestroyInstance();
-}
--- Client/Code/MainGame.h
+++ Client/Code/MainGame.h
@@ -1,25 +1,7 @@
 #pragma once
 
-//class CServerMgr;
 class CMainGame
 {
-private:
-	// (수정) 아래에 있는 것들 다 Mgr로 할 필요없다!
-	// 싱글톤일 이유가 없엉
-	class CDevice*		m_pDevice;
-
-	class CSceneMgr*	m_pSceneMgr;
-	class CTimer*		m_pTimer;
-
-	class CKeyMgr*		m_pKeyMgr;
-	//CServer*		m_pServerMgr;
-
-public:
-	HRESULT		Init();
-	void		Update();
-	void		Render();
-	void		Release();
-
 public:
 	CMainGame();
 	~CMainGame();
